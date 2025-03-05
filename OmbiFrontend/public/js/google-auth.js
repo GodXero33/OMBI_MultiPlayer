95bb86b01c6885da37895a09dde71958801da0d9
GodXero <shansathish33@gmail.com>
2025-03-05 23:58:32 +0530

    front end angular setup

--- OmbiFrontend/public/js/google-auth.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-	apiKey: "AIzaSyCwdOCc4KKtxvOeeje1ut5olE8JSOf3s1g",
-	authDomain: "ombiarenalk.firebaseapp.com",
-	projectId: "ombiarenalk",
-	storageBucket: "ombiarenalk.firebasestorage.app",
-	messagingSenderId: "741009182899",
-	appId: "1:741009182899:web:fa87bb198f2123a58420c6",
-	measurementId: "G-YFJYEECH3C"
-};
