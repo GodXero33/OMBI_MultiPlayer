ba08244d0b09f1680c24892a0d511c57894a3d01
GodXero <shansathish33@gmail.com>
2025-03-05 21:51:23 +0530

    front end server logs, and cards images

--- OmbiFrontend/server.js
+++ OmbiFrontend/server.js
@@ -2,12 +2,29 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
-// Use PORT from environment variables or default to 5500
 const PORT = process.env.PORT || 5500;
 
-// Middlewares
-app.use(express.static("public"));
+// Middleware to log requests
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next();
+});
+
+// Disable caching
+app.use((req, res, next) => {
+	res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+	res.setHeader("Pragma", "no-cache");
+	res.setHeader("Expires", "0");
+	next();
+});
+
+// Serve static files
+app.use(express.static("public", {
+	etag: false,
+	lastModified: false,
+	cacheControl: false
+}));
+
 app.use(cors());
 
 // Start the server
