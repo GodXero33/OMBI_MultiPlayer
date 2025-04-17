44630de01da1d8631c684235e3efdaa532b4f123
GodXero <godxero.dev@gmail.com>
2025-04-17 23:29:40 +0530

    start web socket game server over again to make one server for game and one serer for lobby just for simplify things

--- OmbiWSServer/src/models/lobby-player.js
+++
@@ -1,8 +0,0 @@
-export class LobbyPlayer {
-	constructor (ws, lobbyPlayerInfo) {
-		this.ws = ws;
-		this.id = lobbyPlayerInfo.playerId;
-		this.name = lobbyPlayerInfo.playerName;
-		this.inMatch = false;
-	}
-}
